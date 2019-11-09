1102667e07979550449669841eef6802475a33a5
Felipo-Antunes <felipeantunes2021@gmail.com>
2019-11-08 23:04:41 -0300

    aula 05

---
+++ aula_05/variaveis.php
@@ -0,0 +1,34 @@
+
+<?php
+
+  $titulo = "Dinâmico";
+  $inteiro = 50;
+  $reais = 39.55;
+  $string = "Felipe"
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+	<meta charset="utf-8">
+	<title>Variáveis</title>
+	<link rel="stylesheet" type="text/css" href="scss/estilo.css">
+
+</head>
+<body>
+
+  <h1>Titulo: <?php echo $titulo; ?></h1>
+
+  <h2>Informações das variaveis:</h2>
+
+  <p>$inteiro: <?php var_dump($inteiro); ?></p>
+
+  <p>$reais: <?php var_dump($reais); ?></p>
+
+  <p>$string: <?php var_dump($string); ?></p>
+
+  <a href="index.html">Aula 05</a><br>
+</body>
+</html>
